5107993c882cf555ec4ab59fd9782e59ee12bf37
alemohamad <alejandro.mohamad@gmail.com>
2013-11-04 11:05:50 -0300

    Added feed meta tag shortcut.

--- application/views/layouts/application.php
+++ application/views/layouts/application.php
@@ -6,6 +6,8 @@
         <title>CodeIgniter Project Starter</title>
         <meta name="description" content="CodeIgniter Project Starter">
 
+        <link rel="alternate" href="<?php echo site_url('feed'); ?>" title="Atom" type="application/atom+xml">
+
         <link rel="shortcut icon" href="<?php echo base_url('assets/img/favicon.ico'); ?>">
         <link rel="apple-touch-icon-precomposed" href="<?php echo base_url('assets/img/apple-touch-icon-precomposed.png'); ?>">
 
